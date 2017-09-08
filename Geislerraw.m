33f3078a14a3c964cd5f7432e49fba073a8ce53c
qtomcat <vay.tomcat@gmail.com>
2017-09-08 18:12:28 +0100

    bugs fixed

--- Geislerraw.m
+++ Geislerraw.m
@@ -1,8 +1,11 @@
-addpath('p1');
-addpath('p1');
-addpath(genpath('MDP'));
-addpath(genpath('MFDFA'));
-addpath(genpath('mfanalysis.m'));
+addpath(genpath('p1'));
+addpath(genpath('p2'));
+addpath('MFDFA');
+%enable graphics for md-dfa
+mfdfagr=1;
+
+
+%load data files
 h=load('me.raw.025.txt');
 k=load('me.raw.02.txt');
 l=load('me.raw.015.txt');
@@ -10,24 +13,22 @@ m=load('me.raw.01.txt');
 n=load('raw.mif.01.txt');
 o=load('raw.mif.02.txt');
 p=load('raw.mif.015.txt');
-try
-cd('data&scripts');
-end;
 h=[m;n];
-h=[p;l];
-h=[k;o];
-h=[h];
+%h=[p;l];
+%h=[k;o];
+%h=[h];
 
 h1=h(1:length(h)-1,1);
 h2=h(2:length(h),1);
 dh=h2-h1;
 
-scmin=4;
+scmin=36;
 scmax=400;
-scres=10;
+scres=32;
 exponents=linspace(log2(scmin),log2(scmax),scres);
 scale=round(2.^exponents);
 q=linspace(-10,10,21);
-xtrn=randn(length(Xtraj),1);
-shifts1=abs(dh);
-HqcorX=mfanalysis(shifts1,scale,q,mfdfagr);
+xtrn=randn(length(dh),1);
+shifts1=abs(dh+0.0*xtrn);
+[Hq1,tq,hq,Dq,Fq] = MFDFA1(shifts1,scale,q,1,1);
+%HqcorX=mfanalysis(shifts1,scale,q,mfdfagr);
--- mfanalysis.m
+++ mfanalysis.m
@@ -1,11 +1,11 @@
-function [ O ] = mfanalysis( traj, scale, q, mfdfagr )
+function [ O ] = mfanalysis( traj, scale,scres, q, mfdfagr )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 shifts=traj;
 
     
 [Hq1,tq,hq,Dq,Fq] = MFDFA1(shifts,scale,q,1,0);
-correction=Fq(1,1:10)/sum(Fq(1,1:10));
+correction=Fq(1,1:scres)/sum(Fq(1,1:scres));
 shiftperm=shifts(randperm(length(shifts)));
 [Hq2,tq,hq,Dq,Fq2] = MFDFA1(shiftperm,scale,q,1,0);
 shiftperm1=shifts(randperm(length(shifts)));
@@ -15,40 +15,39 @@ shiftperm2=shifts(randperm(length(shifts)));
 
 
 Faver=(Fq2+Fq3+Fq4)/3;
-correction2=Faver(1,1:10)/sum(Faver(1,1:10));
+correction2=Faver(1,1:scres)/sum(Faver(1,1:scres));
+
 
 
 
 
 j=1;
-for i=1:9
-    if (correction(i)/correction(i+1)<0.01)||(correction2(i)/correction2(i+1)<0.01)
+for i=1:scres-1
+    if (Fq(1,i)/Fq(1,i+1)<0.05)||(Fq2(1,i)/Fq2(1,i+1)<0.05)||(Fq3(1,i)/Fq3(1,i+1)<0.05)||(Fq4(1,i)/Fq4(1,i+1)<0.05)
     j=i;
     end;
 end;
 
 scmin=scale(j+1);
-scmax=scale(10);
+scmax=scale(scres);
                 
                 %scmax=ceil(mean(aviter));
-scres=10;
+scres=scres-j;
 exponents=linspace(log2(scmin),log2(scmax),scres);
 scale=round(2.^exponents);    
 
-[Hq1,tq,hq,Dq,Fq] = MFDFA1(shifts,scale,q,1,0 );
+[Hq1,tq,hq,Dq,Fq] = MFDFA1(shifts,scale,q,1,1 );
 
-[Hq2,tq,hq,Dq,Fq2] = MFDFA1(shiftperm,scale,q,1,0);
+[Hq2,tq,hq,Dq,Fq2] = MFDFA1(shiftperm,scale,q,1,1);
 
-[Hq3,tq,hq,Dq,Fq3] = MFDFA1(shiftperm1,scale,q,1,0);
+[Hq3,tq,hq,Dq,Fq3] = MFDFA1(shiftperm1,scale,q,1,1);
 
-[Hq4,tq,hq,Dq,Fq4] = MFDFA1(shiftperm2,scale,q,1,0);
+[Hq4,tq,hq,Dq,Fq4] = MFDFA1(shiftperm2,scale,q,1,1);
 
 
 
 figure;
 plot(Hq1-(Hq2+Hq3+Hq4)/3);
-drawnow;
-
 O=Hq1-(Hq2+Hq3+Hq4)/3;
 end
 
